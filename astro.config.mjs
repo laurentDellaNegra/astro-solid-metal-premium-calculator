f2a653576f788ad85a1d5768dcc2f0edc3da98fe
laurentDellaNegra <laurent.dellanegra@gmail.com>
2023-01-29 14:59:06 +0100

    💄 Customize tailwind and add bg pattern

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
+import solidJs from '@astrojs/solid-js'
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
-});
+  integrations: [
+    tailwind({
+      config: { applyBaseStyles: false },
+    }),
+    solidJs(),
+  ],
+})
